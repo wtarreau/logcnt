3cdd23f14f3724fbf177489828160203b786047c
Willy Tarreau <w@1wt.eu>
2023-09-17 16:15:32 +0200

    logcnt: add a simple log counter
    For now it just counts messages, volume and losses/loops in case a counter
    is provided as the message's body.

---
+++ Makefile
@@ -0,0 +1,43 @@
+TOPDIR     := $(PWD)
+DESTDIR    :=
+PREFIX     := /usr/local
+LIBDIR     := $(PREFIX)/lib
+
+CROSS_COMPILE :=
+
+CC         := $(CROSS_COMPILE)gcc
+OPT_CFLAGS := -O3
+CPU_CFLAGS := -fomit-frame-pointer
+DEB_CFLAGS := -Wall -g
+DEF_CFLAGS :=
+USR_CFLAGS :=
+INC_CFLAGS :=
+THR_CFLAGS :=
+CFLAGS     := $(OPT_CFLAGS) $(CPU_CFLAGS) $(DEB_CFLAGS) $(DEF_CFLAGS) $(USR_CFLAGS) $(INC_CFLAGS) $(THR_CFLAGS)
+
+LD         := $(CC)
+DEB_LFLAGS := -g
+USR_LFLAGS :=
+LIB_LFLAGS := -lm -pthread
+THR_LFLAGS :=
+LDFLAGS    := $(DEB_LFLAGS) $(USR_LFLAGS) $(LIB_LFLAGS) $(THR_LFLAGS)
+
+AR         := $(CROSS_COMPILE)ar
+STRIP      := $(CROSS_COMPILE)strip
+INSTALL    := install
+BINS       := src/logcnt
+OBJS       :=
+OBJS       += $(patsubst %.c,%.o,$(wildcard src/*.c))
+OBJS       += $(patsubst %.S,%.o,$(wildcard src/*.S))
+
+all: tools
+
+tools: $(BINS)
+
+install: tools
+	$(STRIP) $(BINS)
+	[ -d "$(DESTDIR)$(PREFIX)/bin/." ] || mkdir -p -m 0755 $(DESTDIR)$(PREFIX)/bin
+	$(INSTALL) -m 0755 -t $(DESTDIR)$(PREFIX)/bin $(BINS)
+
+clean:
+	-rm -f $(BINS) $(OBJS) *.o *~
---
+++ src/logcnt.c
@@ -0,0 +1,261 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <time.h>
+
+#define MAX_THREADS 64
+
+struct thread_data {
+	pthread_t pth;
+	int id;           /* thread num    */
+	int fd;           /* socket        */
+	long long count;  /* rcvd messages */
+	long long bytes;  /* rcvd bytes    */
+	long long loops;  /* number of loops back */
+	long long losses; /* sum of holes */
+} __attribute__((aligned(64)));
+
+
+void die(int err, const char *msg)
+{
+	fprintf(stderr, "%s\n", msg);
+	exit(err);
+}
+
+void die_err(int err, const char *msg)
+{
+	perror(msg);
+	exit(err);
+}
+
+/* read uint between ptr and end. Stops at the first non-digit */
+unsigned int read_uint(const char *ptr, const char *end)
+{
+	unsigned int i = 0;
+	unsigned int j, k;
+
+	while (ptr < end) {
+		j = *ptr - '0';
+		k = i * 10;
+		if (j > 9)
+			break;
+		i = k + j;
+		ptr++;
+	}
+	return i;
+}
+
+/* mix input data in a xorshift */
+unsigned int small_hash(const void *input, int len)
+{
+	const void *end = input + len;
+	unsigned int x = ~0U/3; // set half bits
+	unsigned int c;
+
+	while (input + 4 <= end) {
+#if defined(__x86_64__) || defined(__i386__) || defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || defined (__aarch64__) || defined(__ARM_ARCH_8A)
+		c = *(unsigned int *)input;
+#else
+		c = ((unsigned char*)input)[0] +
+		    ((unsigned char*)input)[1] * 256 +
+		    ((unsigned char*)input)[2] * 65536 +
+		    ((unsigned char*)input)[3] * 16777216;
+#endif
+		x ^= x << 13;
+		x ^= x >> 17;
+		x ^= x << 5;
+		x ^= c;
+		input += 4;
+	}
+
+	while (input < end) {
+		c = ((unsigned char*)input)[0];
+		x ^= x << 13;
+		x ^= x >> 17;
+		x ^= x << 5;
+		x ^= c;
+		input += 1;
+	}
+
+	return x;
+}
+
+void *udprx(void *arg)
+{
+	struct thread_data *td = arg;
+	int fd = td->fd;
+	ssize_t len;
+	char buffer[65536];
+	struct sockaddr_in addr;
+	socklen_t salen;
+	char *p1 = buffer, *p0, *e;
+	unsigned int hash;
+	unsigned int counter, prev_counter;
+
+	prev_counter = 0;
+	while (1) {
+		salen = sizeof(addr);
+		len = recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&addr, &salen);
+		if (len < 0)
+			break;
+
+		td->count++;
+		td->bytes += len;
+
+		/* skip priority */
+		p1 = buffer;
+		e = buffer + len;
+		if (*p1 == '<') {
+			if (p1[1] == '>')
+				p1 += 2;
+			else if (p1[2] == '>')
+				p1 += 3;
+			else if (p1[3] == '>')
+				p1 += 4;
+			else if (p1[4] == '>')
+				p1 += 5;
+			else {
+				while (p1 < e && *p1 != ' ')
+					p1++;
+			}
+			if (*p1 == ' ')
+				p1++;
+		}
+
+		/* we're now on first char after the space after priority. The format
+		 * is |mon dd hh:mm:ss[.*]|
+		 *      3   2     8  + 2 spaces = 15 chars
+		 */
+		p1 += 3 + 1 + 2 + 1 + 8;
+		if (p1 >= e)
+			continue;
+
+		/* skip optional subsecond chars */
+		while (p1 < e && *p1 != ' ')
+			p1++;
+
+		if (*p1 == ' ')
+			p1++;
+
+		/* now p1 is on the hostname. If it ends with a colon it's the tag
+		 * and we have no host name.
+		 */
+		for (p0 = p1; p1 < e && *p1 != ' '; p1++)
+			;
+
+		if (*(p1 - 1) == ':') {
+			/* no hostname, hash src IP */
+			hash = small_hash(&addr,  salen);
+		} else {
+			/* hostname, hash it */
+			hash = small_hash(p0, p1 - p0);
+
+			/* now also skip tag */
+			if (*p1 == ' ')
+				p1++;
+
+			for (p0 = p1; p1 < e && *p1 != ' '; p1++)
+				;
+		}
+
+		/* we're now past the tag */
+		if (*p1 == ' ')
+			p1++;
+
+		/* the beginning of the message is expected to be a counter */
+		counter = read_uint(p1, e);
+		if (counter < prev_counter)
+			td->loops++;
+		else if (counter > prev_counter + 1)
+			td->losses += counter - prev_counter - 1;
+	}
+
+	close(fd);
+	pthread_exit(NULL);
+}
+
+
+int main(int argc, char **argv)
+{
+	struct thread_data td[MAX_THREADS];
+	struct sockaddr_in saddr;
+	long long total_msgs = 0;
+	long long total_bytes = 0;
+	long long total_losses = 0;
+	long long total_loops = 0;
+	int tot_time = 0;
+	int num_threads = 1;
+	int port = 514;
+	int i, fd;
+
+	if (argc > 1) {
+		num_threads = atoi(argv[1]);
+		if (num_threads < 0 || num_threads > MAX_THREADS)
+			die(1, "Invalid number of threads\n");
+		argc--; argv++;
+	}
+
+	if (argc > 1) {
+		port = atoi(argv[1]);
+		if (port < 0 || port > 65535)
+			die(1, "Invalid port number\n");
+		argc--; argv++;
+	}
+
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sin_family = AF_INET;
+	saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	saddr.sin_port = htons(port);
+
+	for (i = 0; i < num_threads; i++) {
+		const int one = 1;
+
+		fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+		if (fd < 0)
+			die_err(1, "socket");
+
+		setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+		setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, &one, sizeof(one));
+
+		if (bind(fd, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
+			die_err(1, "bind");
+
+		td[i].id = i;
+		td[i].fd = fd;
+	}
+
+	for (i = 0; i < num_threads; i++)
+		pthread_create(&td[i].pth, NULL, udprx, (void *)&td[i]);
+
+	printf("  time   totmsg    totbytes   totloss totloops |  msg/s   bytes/s loss/s loops/s\n");
+	while (1) {
+		long long prev_msgs = total_msgs;
+		long long prev_bytes = total_bytes;
+		long long prev_losses = total_losses;
+		long long prev_loops = total_loops;
+
+		sleep(1);
+		tot_time++;
+		total_msgs = total_bytes = total_losses = total_loops = 0;
+		for (i = 0; i < num_threads; i++) {
+			total_msgs += td[i].count;
+			total_bytes += td[i].bytes;
+			total_losses += td[i].losses;
+			total_loops += td[i].loops;
+		}
+
+		printf("%6d %lld %lld %lld %lld | %lld %lld %lld %lld\n",
+		       tot_time, total_msgs, total_bytes, total_losses, total_loops,
+		       total_msgs-prev_msgs, total_bytes-prev_bytes,
+		       total_losses-prev_losses, total_loops-prev_loops);
+	}
+
+	close(fd);
+	return 0;
+}
